09764dfc7bdb3d5333293b0400a926470db824d5
Lê Sĩ Bích <moonlight8978@gmail.com>
2019-05-29 12:12:22 +0700

    Splash screen (#9)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { StyleSheet, View, AsyncStorage } from 'react-native'
-import { Font, SplashScreen } from 'expo'
+import { Font, SplashScreen, Asset } from 'expo'
 import Sentry from 'sentry-expo'
+import {
+  FontAwesome,
+  AntDesign,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from '@expo/vector-icons'
 
 import { withDb } from './db'
 import MNG from './routes'
@@ -13,20 +19,47 @@ const styles = StyleSheet.create({
   },
 })
 
-function loadFonts() {
-  /* eslint-disable global-require */
-  return Font.loadAsync({
-    'OpenSans-Bold': require('../assets/fonts/OpenSans/OpenSans-Bold.ttf'),
+const allFonts = [
+  { 'OpenSans-Bold': require('../assets/fonts/OpenSans/OpenSans-Bold.ttf') },
+  {
     'OpenSans-BoldItalic': require('../assets/fonts/OpenSans/OpenSans-BoldItalic.ttf'),
+  },
+  {
     'OpenSans-ExtraBold': require('../assets/fonts/OpenSans/OpenSans-ExtraBold.ttf'),
+  },
+  {
     'OpenSans-ExtraBoldItalic': require('../assets/fonts/OpenSans/OpenSans-ExtraBoldItalic.ttf'),
+  },
+  {
     'OpenSans-Italic': require('../assets/fonts/OpenSans/OpenSans-Italic.ttf'),
-    'OpenSans-Light': require('../assets/fonts/OpenSans/OpenSans-Light.ttf'),
+  },
+  { 'OpenSans-Light': require('../assets/fonts/OpenSans/OpenSans-Light.ttf') },
+  {
     'OpenSans-LightItalic': require('../assets/fonts/OpenSans/OpenSans-LightItalic.ttf'),
+  },
+  {
     'OpenSans-Regular': require('../assets/fonts/OpenSans/OpenSans-Regular.ttf'),
+  },
+  {
     'OpenSans-SemiBold': require('../assets/fonts/OpenSans/OpenSans-SemiBold.ttf'),
+  },
+  {
     'OpenSans-SemiBoldItalic': require('../assets/fonts/OpenSans/OpenSans-SemiBoldItalic.ttf'),
-  })
+  },
+  FontAwesome.font,
+  AntDesign.font,
+  MaterialCommunityIcons.font,
+  MaterialIcons.font,
+]
+
+const allImages = [require('../assets/drawer-header.png')]
+
+function loadFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font))
+}
+
+function loadImages(images) {
+  return images.map(image => Asset.fromModule(image).downloadAsync())
 }
 
 class Bootstrap extends React.Component {
@@ -42,11 +75,10 @@ class Bootstrap extends React.Component {
 
   async componentDidMount() {
     try {
-      await loadFonts()
+      await Promise.all([...loadFonts(allFonts), ...loadImages(allImages)])
       const db = await AsyncStorage.getItem('db')
       this.props.setDb(db ? JSON.parse(db) : {})
-      this.setState({ isReady: true })
-      SplashScreen.hide()
+      this.setState({ isReady: true }, SplashScreen.hide)
     } catch (error) {
       Sentry.captureException(error)
       this.props.toast.push({ message: error.message })
